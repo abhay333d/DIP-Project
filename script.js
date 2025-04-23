5f5883611a33f0e7699cdf9a77f54d86f8f992ac
abhay333d <abhaydandge333@gmail.com>
2025-04-23 20:49:46 +0530

    imported the model

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     scene.add(light);
 
+    const robot = await loadGLTF(
+      "./assets/models/RobotExpressive.glb"
+    );
+    robot.scene.scale.set(0.2, 0.2, 0.2);
+    robot.scene.position.set(0, -0.2, 0);
+
     
   };
   start();
